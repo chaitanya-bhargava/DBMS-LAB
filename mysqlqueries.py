aca97e9913f63bec76b3f42b06ef02fde1191e21
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2022-10-04 14:12:28 +0530

    natural join

--- mysqlqueries.py
+++ mysqlqueries.py
@@ -3,12 +3,7 @@ import datetime
 con=c.connect(host="localhost",user="root",password="",database="csai")
 if con.is_connected() :
     mycursor=con.cursor()
-    mycursor.execute("SELECT * FROM Employee")
-    myresult=mycursor.fetchall()
-    for i in myresult:
-        print(i)
-    print("\n")
-    mycursor.execute("SELECT Dname,Empid,DATE_FORMAT(DDOB,'%d-%m-%Y'),DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),DDOB)),'%Y')+0 AS no_of_years,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),DDOB)),'%c')+0 AS no_of_months,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),DDOB)),'%d')+0 AS no_of_days FROM Dependants")
+    mycursor.execute("SELECT empid,empname,address,Dname,DATE_FORMAT(DDOB,'%d-%m-%Y'),DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),DDOB)),'%Y')+0 AS no_of_years,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),DDOB)),'%c')+0 AS no_of_months,DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),DDOB)),'%d')+0 AS no_of_days FROM Employee NATURAL JOIN Dependants ORDER BY empid")
     myresult=mycursor.fetchall()
     for j in myresult:
         print(j)
